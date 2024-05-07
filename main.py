7b055c3e81a3d020208336a1aca2012e8947f872
Deus <119631661+deuskarao@users.noreply.github.com>
2024-05-07 19:18:02 +0300

    Update Version 1.1

--- main.py
+++ main.py
@@ -3,51 +3,77 @@ import qrcode
 from time import sleep, strftime, localtime
 
 
-# Creating a new Secret Key specific for you
-# everyone has to have different one
-# secret = pyotp.random_base32()
+class Auth:
+    def __init__(self) -> None:
+        self.currentDate = strftime("%d-%m-%Y - %H-%M-%S", localtime())
+        self.secret = ""
+        self.key = ""
 
-# Saving the Secret Key that just created
-# key = open("mySecret", "w")
-# secret_save = key.write(secret)
-# key.close()
+    def create_otp(self):
+        # Creating one time password and saving logs
+        self.onetime = pyotp.TOTP(self.secret)
+        self.onetime_now = str(self.onetime.now())
+        print(self.onetime_now)
 
-# Using the same Secret Key if you have one
-key = open("mySecret", "r")
-secret = key.read()
-key.close()
+    def create_pass(self):
+        # Creating a new Secret Key specific for you
+        self.secret = pyotp.random_base32()
 
-# Creating one time password and saving logs
-onetime = pyotp.TOTP(secret)
-onetime_now = str(onetime.now())
+        # Saving the Secret Key that just created
+        self.key = open("mySecret.txt", "w")
+        self.key.write(self.secret)
+        self.key.close()
 
-currentDate = strftime("%d-%m-%Y - %H-%M-%S", localtime())
+    def get_sec(self):
+        # Using the same Secret Key if you have one
+        self.key = open("mySecret.txt", "r")
+        self.secret = self.key.read()
+        self.key.close()
 
-# Getting Credentials
-get_user = input("Username : ")
-get_pass = input("Password : ")
-get_auth = input("2FA Code : ")
+    def get_cred(self):
+        # Getting Credentials
+        self.get_user = input("Username : ")
+        self.get_pass = input("Password : ")
+        self.get_auth = input("2FA Code : ")
 
-twofa = open("keyLogs", "a")
-timedCode = twofa.write(f"{currentDate} | {get_user} | {onetime_now}" + "\n")
-twofa.close()
+        twofa = open("keyLogs", "a")
+        timedCode = twofa.write(f"{self.currentDate} | {self.get_user} | {self.onetime_now}" + "\n")
+        twofa.close()
 
-# Creating QR Based on the one time Password
-qrcode.make(onetime).save(f"{get_user}.png")
+    def create_qr(self):
+        # Creating QR Based on the one time Password
+        qrcode.make(self.onetime).save(f"{self.get_user}.png")
 
-trying = 0
-while True:
-    if not onetime.verify(get_auth):
-        sleep(1)
-        print("\nInvalid!\n")
-        loop_try = input("2FA Code : ")
-        get_auth = loop_try
-        trying += 1
+        trying = 0
+        while True:
+            if not self.onetime.verify(self.get_auth):
+                sleep(1)
+                print("\nInvalid!\n")
+                loop_try = input("2FA Code : ")
+                self.get_auth = loop_try
+                trying += 1
 
-    if trying == 3:
-        print("Enough Guesses")
-        exit()
+            if trying == 3:
+                print("Enough Guesses")
+                exit()
 
-    else:
-        print("\nGood to go..")
-        exit()
+            else:
+                print("\nGood to go..")
+                exit()
+
+    def starter(self):
+        print("""
+1 - Login
+2
+""")
+
+runner = Auth()
+# login
+# if not in database create a new pass
+# if in
+ 
+
+runner.create_otp()
+runner.get_sec()
+runner.get_cred()
+runner.create_qr()
